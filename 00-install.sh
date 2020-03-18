094ab98201c53eadd2a3f3e77ccd31edeb4aeb41
emil torlen <>
2020-03-18 13:52:33 +0100

    changed flannel

--- 00-install.sh
+++ 00-install.sh
@@ -1,12 +1,11 @@
 # https://www.redpill-linpro.com/techblog/2019/04/04/kubernetes-setup.html
 sudo apt-get update
-sudo apt-get install docker.io -y
-sudo systemctl enable docker
-sudo systemctl start docker
 sudo apt-get install curl -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-sudo apt-get install kubeadm kubelet kubectl -y
+sudo apt-get install kubeadm kubelet kubectl jq docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
 sudo apt-mark hold kubeadm kubelet kubectl
 sudo hostnamectl set-hostname master-node
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
@@ -14,5 +13,5 @@ sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
+kubectl taint nodes --all node-role.kubernetes.io/master-
