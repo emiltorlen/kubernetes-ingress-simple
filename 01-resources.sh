2f4f9450bf176fc664f3643903166b7267bf25c7
emil torlen <>
2020-03-18 13:02:54 +0100

    init

---
+++ 01-resources.sh
@@ -0,0 +1,7 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
+kubectl apply -f apple.yaml
+kubectl apply -f banana.yaml
+kubectl create -f ingress.yaml
+kubectl get pods --all-namespaces -l app=ingress-nginx
+
